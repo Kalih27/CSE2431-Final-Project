4e7e156fe2d6e380c823bff63e09df5a1449f3d3
Kalih27 <zpqm1065@gmail.com>
2018-11-14 17:58:32 -0500

    updated logger.c
    added functionality for reading from /proc/<pid>/stat.
    must be run as sudo
    has bugs

--- logger.c
+++ logger.c
@@ -3,6 +3,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <signal.h>
+
+volatile int alarmFlag = 0;
+
+void alarmHandler(int sig){
+	alarmFlag = 1;
+}
+
 int main() {
 	//file pointer for log file and read file
 	FILE *logFile, *readFile;
@@ -11,18 +19,85 @@ int main() {
 	int neededPID;
 
 	//string to hold full file path
-	char *filePath;
+	char filePath[100];
+
+	//longs to hold utime and stime (both are clock ticks)
+	long utime, stime;
+	//longs to hold last utime and last stime
+	long lutime, lstime;
 	
 	//get pid from user
 	printf("Please enter PID of process to measure: ");
 	scanf("%d", &neededPID);
-	
+	//puts("s\n");
+
 	//create string for file path
 	sprintf(filePath, "/proc/%d/stat", neededPID);
 	
 	//print file path before trying to open file
 	printf("%s\n", filePath);
+	
+	//puts("l");
+	//open file to log data too
+	logFile = fopen("log.txt", "w");
+	if(logFile == NULL){
+		printf("Could not open log file\n");
+		return 0;
+	}
 
+	//puts("r");
 	//open /proc/<neededPID> folder
-	//readFile = fopen(filePath, r);
+	readFile = fopen(filePath, "r");
+	if(readFile == NULL){
+		printf("Could not open read file\n");
+		return 0;
+	}
+
+	//signal so that we can use alarm
+	signal(SIGALRM, alarmHandler);
+
+	//puts("a");
+	//initialize alarmFlag to 0
+	alarmFlag = 0;
+	//puts("o");
+	int outerLoop = 0, inner = 0; //loop variable
+	
+	alarm(1);
+	while(outerLoop ==  0){
+		//if alarm goes off
+		if(alarmFlag == 1){
+			printf("Alarm happened!\n");
+
+			//read utume and stime from readFile aka the scariest fscanf in the west
+			fscanf(readFile,"%* %* %* %* %* %* %* %* %* %* %* %* %* %lu %lu %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %*", &utime, &stime);
+
+			//offset to beginning of file
+			rewind(readFile);
+
+			//write number to log file
+			fprintf(logFile, "%lu %lu\n", utime, stime);
+			printf("User Ticks: %lu\tSystem Ticks: %lu\n", utime, stime);
+			if(inner>0){
+				printf("User delta: %lu\tSystem delta: %lu\n", lutime, lstime);
+			}
+
+			//increment inner
+		  	inner++;
+
+			//if inner = 10 (ten 1-second waits) exit loop
+		  	if(inner==10){
+			  outerLoop++;
+			  printf("Leaving loop!\n");
+			}
+
+			//reset alarm flag
+			alarmFlag = 0;
+			alarm(1);
+		}
+	}
+	
+	printf("Bye!\n");
+	//close file
+	fclose(logFile);
+	fclose(readFile);
 }
