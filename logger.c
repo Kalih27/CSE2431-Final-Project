07301622c0e3183df8b97ae51042953402d85fb9
Kalih27 <zpqm1065@gmail.com>
2018-11-14 18:43:29 -0500

    Fixed bugs
    Fixed bugs to make logger.c function properly.

--- logger.c
+++ logger.c
@@ -16,15 +16,17 @@ int main() {
 	FILE *logFile, *readFile;
 	
 	//pid of process to monitor
-	int neededPID;
+	int neededPID=1;
 
 	//string to hold full file path
 	char filePath[100];
 
 	//longs to hold utime and stime (both are clock ticks)
 	long utime, stime;
+	utime=stime=0;
 	//longs to hold last utime and last stime
 	long lutime, lstime;
+	lutime=lstime=0;
 	
 	//get pid from user
 	printf("Please enter PID of process to measure: ");
@@ -35,7 +37,7 @@ int main() {
 	sprintf(filePath, "/proc/%d/stat", neededPID);
 	
 	//print file path before trying to open file
-	printf("%s\n", filePath);
+	printf("Reading from \"%s\"\n", filePath);
 	
 	//puts("l");
 	//open file to log data too
@@ -47,11 +49,11 @@ int main() {
 
 	//puts("r");
 	//open /proc/<neededPID> folder
-	readFile = fopen(filePath, "r");
-	if(readFile == NULL){
-		printf("Could not open read file\n");
-		return 0;
-	}
+	//readFile = fopen(filePath, "r");
+	//if(readFile == NULL){
+		//printf("Could not open read file\n");
+		//return 0;
+	//}
 
 	//signal so that we can use alarm
 	signal(SIGALRM, alarmHandler);
@@ -60,33 +62,46 @@ int main() {
 	//initialize alarmFlag to 0
 	alarmFlag = 0;
 	//puts("o");
-	int outerLoop = 0, inner = 0; //loop variable
+	int outerLoop = 1, inner = 0; //loop variable
 	
 	alarm(1);
-	while(outerLoop ==  0){
+	while(outerLoop){
 		//if alarm goes off
-		if(alarmFlag == 1){
+		if(alarmFlag){
 			printf("Alarm happened!\n");
 
+			//open /proc/<neededPID> folder
+			readFile = fopen(filePath, "r");
+			if(readFile == NULL){
+				printf("Could not open read file\n");
+				return 0;
+			}
+
 			//read utume and stime from readFile aka the scariest fscanf in the west
-			fscanf(readFile,"%* %* %* %* %* %* %* %* %* %* %* %* %* %lu %lu %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %* %*", &utime, &stime);
+			fscanf(readFile,"%*d %*s %*c %*d %*d %*d %*d %*d %*u %*u %*u %*u %*u %lu %lu", &utime, &stime);
 
 			//offset to beginning of file
-			rewind(readFile);
+			//rewind(readFile);
+			fclose(readFile);
 
 			//write number to log file
 			fprintf(logFile, "%lu %lu\n", utime, stime);
 			printf("User Ticks: %lu\tSystem Ticks: %lu\n", utime, stime);
+			//Print difference if applicable
 			if(inner>0){
-				printf("User delta: %lu\tSystem delta: %lu\n", lutime, lstime);
+				printf("User delta: %lu\tSystem delta: %lu\n", utime-lutime, stime-lstime);
 			}
+			//Set last tick count
+			lutime = utime;
+			lstime=stime;
+
 
 			//increment inner
 		  	inner++;
 
 			//if inner = 10 (ten 1-second waits) exit loop
 		  	if(inner==10){
-			  outerLoop++;
+			  outerLoop=0;
 			  printf("Leaving loop!\n");
 			}
 
@@ -99,5 +114,5 @@ int main() {
 	printf("Bye!\n");
 	//close file
 	fclose(logFile);
-	fclose(readFile);
+	//fclose(readFile);
 }
