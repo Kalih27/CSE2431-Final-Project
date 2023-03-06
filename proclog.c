328e1ddcd1f1517ac1001bae5b4723c2f3f9329d
JaChian <author.tle@gmail.com>
2023-03-06 00:51:18 -0500

    fix the kernel version issue (#1)

--- proclog.c
+++ proclog.c
@@ -1,5 +1,6 @@
-//Logger that creates a proc file
-//idea from tldp.org/LDP/lkmpg/2.6/html/index.html
+// Logger that creates a proc file
+// idea from tldp.org/LDP/lkmpg/2.6/html/index.html
+#include <linux/version.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -14,51 +15,59 @@
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Kernel module to log process times");
 
-//size of buffer ~32Kb
-#define PROCFS_MAX_SIZE	32768
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0)
+#define HAVE_PROC_OPS
+#endif
 
-//buffer to hold information from log
+// size of buffer ~32Kb
+#define PROCFS_MAX_SIZE 32768
+
+// buffer to hold information from log
 static char procfs_buffer[PROCFS_MAX_SIZE];
 
-//size of buffer
+// size of buffer
 static unsigned long procfs_buffer_size = 0;
 
-//pointer for buffer location in read
+// pointer for buffer location in read
 static char *buff_ptr;
 
-//struct to hold info about proc file
+// struct to hold info about proc file
 struct proc_dir_entry *log_file;
 
 static int endflag;
 
-static void *proc_seq_start(struct seq_file *s, loff_t *pos){
-	
+static void *proc_seq_start(struct seq_file *s, loff_t *pos)
+{
+
 	printk("Start of sequence read!\n");
-	
+
 	(*pos) = endflag;
 
 	buff_ptr = procfs_buffer + ((*pos) * sizeof(char));
 
-	//if pos is greater than or equal to buffer size then leave sequence read 
-	if((*pos) >= procfs_buffer_size-1 || *buff_ptr == '\0'){
+	// if pos is greater than or equal to buffer size then leave sequence read
+	if ((*pos) >= procfs_buffer_size - 1 || *buff_ptr == '\0')
+	{
 		printk("End sequence read\n");
 		return NULL;
 	}
-	
 
 	printk("Place in buffer is: %Ld\n", (*pos));
 
 	return buff_ptr;
 }
 
-static void *proc_seq_next(struct seq_file *s, void *v, loff_t *pos){
+static void *proc_seq_next(struct seq_file *s, void *v, loff_t *pos)
+{
 	printk("Sequence Next!");
-	char *temp = (char*)v;
-	while((*temp) != '\n'){
-		
+	char *temp = (char *)v;
+	while ((*temp) != '\n')
+	{
+
 		(*pos)++;
 		printk("position increased");
-		if((*pos) >= procfs_buffer_size){
+		if ((*pos) >= procfs_buffer_size)
+		{
 			return NULL;
 		}
 		temp++;
@@ -70,72 +79,86 @@ static void *proc_seq_next(struct seq_file *s, void *v, loff_t *pos){
 	return temp;
 }
 
-static void proc_seq_stop(struct seq_file *s, void *v){
+static void proc_seq_stop(struct seq_file *s, void *v)
+{
 	printk("Sequence stop!");
 	buff_ptr = NULL;
 	printk("Sequence stop 2: electric bugaloo");
 }
 
-static int proc_seq_show(struct seq_file *s, void *v){
+static int proc_seq_show(struct seq_file *s, void *v)
+{
 	printk("Showing value");
-	char *temp = (char*)v;
-	do{
-		seq_putc(s,*temp);
+	char *temp = (char *)v;
+	do
+	{
+		seq_putc(s, *temp);
 		temp++;
-	}while(*temp != '\n');
-	seq_putc(s,'\n');
+	} while (*temp != '\n');
+	seq_putc(s, '\n');
 	return 0;
 }
-
 
 static struct seq_operations proc_seq_ops = {
 	.start = proc_seq_start,
 	.next = proc_seq_next,
 	.stop = proc_seq_stop,
-	.show = proc_seq_show
-};
+	.show = proc_seq_show};
 
-static int procfile_open(struct inode *inode, struct file *file){
+static int procfile_open(struct inode *inode, struct file *file)
+{
 	printk("open procfile");
 	return seq_open(file, &proc_seq_ops);
 }
 
-//function to write to proc file
-static ssize_t procfile_write(struct file *file, const char *buffer, size_t count, loff_t * off){
-	//set buffer size
+// function to write to proc file
+static ssize_t procfile_write(struct file *file, const char *buffer, size_t count, loff_t *off)
+{
+	// set buffer size
 	procfs_buffer_size += count;
-	if(procfs_buffer_size > PROCFS_MAX_SIZE){
+	if (procfs_buffer_size > PROCFS_MAX_SIZE)
+	{
 		procfs_buffer_size = PROCFS_MAX_SIZE;
 		printk("Proc file buffer overflow");
 	}
-	else{
+	else
+	{
 		printk("Buffer size updated to: %lu", procfs_buffer_size);
 	}
 
-	//write data to buffer
-	if(copy_from_user(procfs_buffer+(procfs_buffer_size - count), buffer, count)){
+	// write data to buffer
+	if (copy_from_user(procfs_buffer + (procfs_buffer_size - count), buffer, count))
+	{
 		return -EFAULT;
 	}
 
 	return count;
 }
 
-
-
-//struct that holds what functions run for different aspects of log file
+// struct that holds what functions run for different aspects of log file
+#ifdef HAVE_PROC_OPS
+static const struct proc_ops log_file_fops = {
+	.proc_open = procfile_open,
+	.proc_read = seq_read,
+	.proc_lseek = seq_lseek,
+	.proc_release = seq_release};
+#else
 static const struct file_operations log_file_fops = {
- .owner = THIS_MODULE,
- .open = procfile_open,
- .read = seq_read,
- .write = procfile_write,
- .llseek = seq_lseek,
- .release = seq_release
-};
+	.owner = THIS_MODULE,
+	.open = procfile_open,
+	.read = seq_read,
+	.write = procfile_write,
+	.llseek = seq_lseek,
+	.release = seq_release};
+#endif
 
-int __init init_MyKernelModule(void){
-	//adapted from stackoverflow.com/questions/8516021/proc-create-example-for-kernel-module
+int __init init_MyKernelModule(void)
+{
+	// adapted from stackoverflow.com/questions/8516021/proc-create-example-for-kernel-module
+	// fixed the version issue from https://stackoverflow.com/questions/64931555/how-to-fix-error-passing-argument-4-of-proc-create-from-incompatible-pointer
 	log_file = proc_create("timing_log", 0, NULL, &log_file_fops);
-	if(log_file == NULL){
+	if (log_file == NULL)
+	{
 		return -ENOMEM;
 	}
 	printk("kernel module initialized");
@@ -143,9 +166,10 @@ int __init init_MyKernelModule(void){
 	return 0;
 }
 
-void __exit exit_MyKernelModule(void){
+void __exit exit_MyKernelModule(void)
+{
 
-	remove_proc_entry("timing_log",NULL);
+	remove_proc_entry("timing_log", NULL);
 	printk("exiting kernel module");
 	return;
 }
